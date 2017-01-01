b33b7bfb4ee48a06202d36028bc47efb2fa0d9a0
Yacine O <ouarezkiyacine@hotmail.com>
2017-01-01 01:10:18 +0100

    Commited

--- PublisherDZ/urls.py
+++ PublisherDZ/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from myapp import views
+from PUBLISHER_DZ import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^/', myapp_views.home name="home"),
+    url(r'^/', views.home , name="home"),
 ]
